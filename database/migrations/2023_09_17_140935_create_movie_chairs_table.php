69899f8c0d323f91802b2e814445334d766f9e07
phith75 <phith75@gmail.com>
2023-10-05 14:10:52 +0700

    :)

--- database/migrations/2023_09_17_140935_create_movie_chairs_table.php
+++ database/migrations/2023_09_17_140935_create_movie_chairs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name', 150);
             $table->string('price');
             $table->tinyInteger('type');
+            $table->tinyInteger('status');
             $table->timestamps();
         });
     }
