ea2b4b41e69c9f8ad4baf5df9d6492584a100a53
phith75 <phith75@gmail.com>
2023-11-14 15:16:50 +0700

    date of birth

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -125,7 +125,6 @@ class PaymentController extends Controller
     {
 
         $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
-
         $partnerCode = 'MOMOBKUN20180529';
         $accessKey = 'klm05TvNBzhg7h7j';
         $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
