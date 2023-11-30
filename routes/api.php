f5d05c6965191c1cddeb58c857a55a14b532615f
phith75 <phith75@gmail.com>
2023-11-30 14:22:18 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -98,7 +98,6 @@ Route::post('Payment', [PaymentController::class, 'vnpay_payment']); // thanh to
 
 Route::post('momo_payment', [PaymentController::class, 'momo_payment']); // thanh toán momo
 
-Route::post('getdata/{id}/{coin}', [PaymentController::class, 'getdata']); //napj tien qua momo
 
 
 ///////
