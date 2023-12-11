431965b136e8c2b00fff232d36002d39b10905e4
phith75 <phith75@gmail.com>
2023-12-11 19:01:22 +0700

    V:

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 });
 //////
 Route::get('/film-ratings/{film_id}',[RateStarController::class, 'getRatings']);
-Route::get('film_cinema/{id}', [QuerryController::class, 'film_cinema']);  // Lấy thông tin phim theo rạp
+Route::get('film_cinema/{id}', [QuerryController::class, 'film_cinema']);  // Lấy thông tin phim theo rạp 
 Route::get('time_detail_get_by_id/{id}', [QuerryController::class, 'time_detail_get_by_id']);
 Route::get('check_time_detail_by_film_id/{id_cinema}', [QuerryController::class, 'check_time_detail_by_film_id']); /////
 Route::get('time_detail_film', [QuerryController::class, 'check_time_detail_by_film']); /////
